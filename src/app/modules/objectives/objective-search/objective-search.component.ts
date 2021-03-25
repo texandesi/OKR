17519d2a5b5f8ca422b2cb767e1d53fab71fb1bb
SPai <dev@techniti.com>
2021-03-24 21:32:20 -0500

    Objective foundational components mostly in place

--- src/app/modules/objectives/objective-search/objective-search.component.ts
+++ src/app/modules/objectives/objective-search/objective-search.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, Subject } from 'rxjs';
+
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+
+import { Objective } from '../objective';
+import {ObjectivesDataService} from '../../../services/objectives-data-service.service';
+
 @Component({
   selector: 'app-objective-search',
   templateUrl: './objective-search.component.html',
-  styleUrls: ['./objective-search.component.scss']
+  styleUrls: [ './objective-search.component.scss' ]
 })
 export class ObjectiveSearchComponent implements OnInit {
+  objectives$!: Observable<Objective[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private objectiveService: ObjectivesDataService) {}
 
-  ngOnInit(): void {
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
+  ngOnInit(): void {
+    this.objectives$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.objectiveService.searchObjectives(term)),
+    );
+  }
 }
--- src/app/modules/objectives/objectives-routing.module.ts
+++ src/app/modules/objectives/objectives-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        component: ObjectivesComponent,
+        component: ObjectivesComponent
       },
       {
         path: 'form',
