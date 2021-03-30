2c80e93942aeb46bf4d84cbc57b8df6e1f5d1581
SPai <dev@techniti.com>
2021-03-30 11:05:42 -0500

    Table partially working, except for refresh on add

--- src/app/modules/objectives/objectives/objectives.component.ts
+++ src/app/modules/objectives/objectives/objectives.component.ts
@@ -2,8 +2,8 @@ import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {Objective} from '../objective';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
-import {MatTable} from '@angular/material/table';
-import {ObjectiveListDataSource} from '../../../data-sources/objective-list-datasource';
+import {MatTable, MatTableDataSource} from '@angular/material/table';
+import {ObjectivesDataService} from '../../../services/objectives-data-service.service';
 
 @Component({
   selector: 'app-objectives',
@@ -13,45 +13,62 @@ import {ObjectiveListDataSource} from '../../../data-sources/objective-list-data
 export class ObjectivesComponent implements OnInit, AfterViewInit  {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) myMatTable!: MatTable<Objective>;
-  dataSource : ObjectiveListDataSource;
+  @ViewChild(MatTable) matTable!: MatTable<Objective>;
+
+  dataSource : MatTableDataSource<Objective> = new MatTableDataSource<Objective>();
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'name', 'details', 'delete'];
 
-  constructor(private myDataSource : ObjectiveListDataSource) {
-    this.dataSource = myDataSource;
+  constructor(private objectiveService : ObjectivesDataService) {
+    // this.dataSource = myDataSource;
   }
 
   ngOnInit() :void {
+    // this.matTable.dataSource=this.dataSource;
+    // this.dataSource = new
+    // this.dataSource = ;
     this.getObjectives();
   }
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
-    this.myMatTable.dataSource = this.dataSource;
+    this.matTable.dataSource = this.dataSource;
   }
 
   getObjectives(): void {
-    this.dataSource.getObjectives();
+    console.log('Before getting objectives in data source');
+
+    this.objectiveService.getObjectives()
+      .subscribe(objectives => this.dataSource.data = objectives);
+
+    console.log('After getting objectives in data source');
   }
 
   add(name: string): void {
+    console.log('Before Adding objectives in data source');
     name = name.trim();
-    if (!name) {
-      return;
-    }
-    this.dataSource.addObjective(name);
+    if (!name) { return; }
+    this.objectiveService.addObjective({ name } as Objective)
+      .subscribe(objective => {
+        this.dataSource.data.push(objective);
+      });
+
+    this.matTable.renderRows();
+    // this.matTable.dataSource = this.dataSource;
+    // this.matTable.renderRows();
+
+    console.log('After Adding objectives in data source');
 
   }
 
   // TODO Implement back the page refresh and delete functionality for the Objectives list.
   delete(objective: Objective): void {
     this.dataSource.data = this.dataSource.data.filter(h => h !== objective);
-    this.dataSource.deleteObjective(objective.id);
+    // this.dataSource.deleteObjective(objective.id);
 
-    this.getObjectives();
+    // this.getObjectives();
   }
 
 }
