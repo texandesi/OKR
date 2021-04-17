89fd3f2a8daef64a34f1420942837c608940bd95
Sanjeev Pai <spai@techniti.com>
2021-04-17 11:41:07 -0500

    Freezing Objective code for now

--- src/app/modules/objectives/objectives-routing.module.ts
+++ src/app/modules/objectives/objectives-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        component: ObjectiveListComponent,
+        redirectTo: 'nav',
         pathMatch: 'full'
       },
       {
