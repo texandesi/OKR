1b35dcfe1892346de1c51580116ee84dc6e308ac
Sanjeev Pai <sanjeev@rishicorp.com>
2024-04-26 12:45:04 -0500

    Fixed script to add files
    Signed-off-by: Sanjeev Pai <sanjeev@rishicorp.com>

--- scripts/gadd.sh
+++ scripts/gadd.sh
@@ -1,25 +1,36 @@
-#!/bin/zsh
-#
-#
+#!/bin/bash
+##################################-BEGIN-################################################
+#																						#
+# Shell script to quickly add files to a specified branch with the specified message	#
+# 																						#
+# usage: gadd.sh commit-message [commit-branch]											#
+#																						#
+#########################################################################################
 
+#identify current branch
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
-#echo "First string is '$1'"
-
+# check if at least the message have been
 if [[ -z "$1"  ]]; then
-	echo "usage: gadd.sh [commit-message] [commit-branch]"
+	echo "usage: gadd.sh commit-message [commit-branch]"
 	exit 2
 fi
 
+#switch to the provided branch
 if [[ "$2" -eq "" ]]; then
 	TARGET_BRANCH=$CURRENT_BRANCH
 else
 	TARGET_BRANCH=$2
 fi
 
+#do a signed commit to the provided branch
 git switch $TARGET_BRANCH
 
 git add .
 git commit -S -m $1
 git push 
 
+# restore original branch
+git switch $CURRENT_BRANCH
+
+###########################-END-#########################################################
