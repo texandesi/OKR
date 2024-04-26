c70726bde63c62a552fa6fd7fb7607f5bfde0b90
Sanjeev Pai <sanjeev@rishicorp.com>
2024-04-25 22:17:20 -0500

    test message

--- scripts/gadd.sh
+++ scripts/gadd.sh
@@ -4,11 +4,20 @@
 
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
+#echo "First string is '$1'"
+
+if [[ -z "$1"  ]]; then
+	echo "usage: gadd.sh [commit-message] [commit-branch]"
+	exit 2
+fi
+
 if [[ "$2" -eq "" ]]; then
 	TARGET_BRANCH=$CURRENT_BRANCH
 else
 	TARGET_BRANCH=$2
 fi
+
+git switch $TARGET_BRANCH
 
 git add .
 git commit -S -m $1
