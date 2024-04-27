5849ad6bf5e5f8befd254b666f240d751f3f7a4a
Sanjeev Pai <sanjeev@rishicorp.com>
2024-04-27 12:54:20 -0500

    Fixed shell script. Added polyfills.ts back, added enty to gitignore
    Signed-off-by: Sanjeev Pai <sanjeev@rishicorp.com>

--- scripts/startservers.sh
+++ scripts/startservers.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+##################################-BEGIN-################################################
+#																						                                            #
+# Shell script to start the servers for the development to start                        #
+# Paths are assumed relative to the application home from where the script will run     #
+#																						                                            #
+# usage: startserevers.sh django-app-path angular-app-path					                    #
+#																						                                            #
+#########################################################################################
+
+APP_HOME=`pwd`
+DJANGO_APP_LOCATION=${APP_HOME}/$1
+ANGULAR_APP_LOCATION=${APP_HOME}/$2
 
 #echo starting Mysql server
 
@@ -9,21 +21,29 @@ else
 	echo "mysql already started"
 fi
 
-if [[ -n "${DJANGO_APP_LOCATION}" ]]; then
+if [[ -n "$1" ]]; then
 	echo starting Django server located at "${DJANGO_APP_LOCATION}"
 
 	cd ${DJANGO_APP_LOCATION}
-	python manage.py runserver
+	nohup python manage.py runserver &
+
+  if [[ -n "$2" ]]; then
+    echo starting Angular server located at "${ANGULAR_APP_LOCATION}"
+
+    cd ${ANGULAR_APP_LOCATION}
+    nohup ng serve &
+  else
+    echo "variable ANGULAR_APP_LOCATION needs to be set to the path of the Angular application"
+
+    # kill the Django server
+    ps ax | grep "manage.py" | grep -v "grep" | awk {'print $1'} | xargs kill
+
+    exit 2
+  fi
+
 else
-	echo "variable DJANGO_APP_LOCATION needs to be set to the application path"
+	echo "usage: startserevers.sh django-app-path angular-app-path"
+  exit 2
 fi
 
-if [[ -n "${ANGULAR_APP_LOCATION}" ]]; then
-	echo starting Angular server located at "${ANGULAR_APP_LOCATION}"
-
-	#cd ${ANGULAR_APP_LOCATION}
-	#python manage.py runserver
-else
-	echo "variable ANGULAR_APP_LOCATION needs to be set to the application path"
-fi
 
