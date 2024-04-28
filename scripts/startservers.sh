0cfcf08feea07d4d71ccfcda19001a8e04cd7aed
Sanjeev Pai <sanjeev@rishicorp.com>
2024-04-27 21:20:35 -0500

    Working on improving startservers shell script
    Signed-off-by: Sanjeev Pai <sanjeev@rishicorp.com>

--- scripts/startservers.sh
+++ scripts/startservers.sh
@@ -8,12 +8,77 @@
 #																						                                            #
 #########################################################################################
 
+#TODO - Change this shell script to use reusable functions and other scripts like stopserver.sh
+
 APP_HOME=`pwd`
-DJANGO_APP_LOCATION=${APP_HOME}/$1
-ANGULAR_APP_LOCATION=${APP_HOME}/$2
+DJANGO_APP_LOCATION=${APP_HOME}/back-end
+ANGULAR_APP_LOCATION=${APP_HOME}/front-end
 
-#echo starting Mysql server
+if [[ -n "$1" ]]; then
+  DJANGO_APP_LOCATION=${APP_HOME}/$1
 
+  #check if the directory exists
+  if [[ -d ${DJANGO_APP_LOCATION} ]]; then
+
+    #check of the executable exists
+    if ! [[ -f DJANGO_APP_LOCATION/manage.py ]]
+      echo "Django executable not found in directory ${DJANGO_APP_LOCATION}"
+      exit 2
+    else
+      echo "Starting Django server in directory ${DJANGO_APP_LOCATION}"
+    fi
+
+	  cd ${DJANGO_APP_LOCATION}
+  else
+     echo "Django directory not found at ${DJANGO_APP_LOCATION}"
+     exit 2
+  fi
+
+else
+	echo "usage: startserevers.sh django-app-path angular-app-path"
+  echo "stopping mysql and existing instances of Django and Angular"
+
+  # kill the Django server and angular server if they are running
+  ps ax | grep "manage.py" | grep -v "grep" | awk {'print $1'} | xargs kill
+  ps ax | grep "ng serve" | grep -v "grep" | awk {'print $1'} | xargs kill
+
+  brew services stop mysql
+  exit 2
+fi
+
+if [[ -n "21" ]]; then
+  ANGULAR_APP_LOCATION=${APP_HOME}/$2
+
+  #check if the directory exists
+  if [[ -d ${ANGULAR_APP_LOCATION} ]]; then
+
+    #check of the executable exists
+    if ! [[ -f ANGULAR_APP_LOCATION/ng ]]
+      echo "Angular executable not found in directory ${ANGULAR_APP_LOCATION}"
+      exit 2
+    else
+      echo "Starting Angular server in directory ${ANGULAR_APP_LOCATION}"
+    fi
+
+	  cd ${ANGULAR_APP_LOCATION}
+  else
+     echo "Angular directory not found at ${ANGULAR_APP_LOCATION}"
+     exit 2
+  fi
+
+else
+	echo "usage: startserevers.sh [django-app-path] [angular-app-path]"
+  echo "stopping mysql and existing instances of Django and Angular"
+
+  # kill the Django server and angular server if they are running
+  ps ax | grep "manage.py" | grep -v "grep" | awk {'print $1'} | xargs kill
+  ps ax | grep "ng serve" | grep -v "grep" | awk {'print $1'} | xargs kill
+
+  brew services stop mysql
+  exit 2
+fi
+
+#start Django and Angular
 if [[ `brew services list | grep mysql | awk '{ print $2}'` == 'none' ]]; then
 	echo starting Mysql server
 	brew services start mysql
@@ -21,32 +86,10 @@ else
 	echo "mysql already started"
 fi
 
-if [[ -n "$1" ]]; then
-	cd ${DJANGO_APP_LOCATION}
-  echo "Starting Django server in directory ${DJANGO_APP_LOCATION}"
+nohup python manage.py runserver &
+nohup ng serve &
 
-  nohup python manage.py runserver &
 
-  if [[ -n "$2" ]]; then
-    cd ${ANGULAR_APP_LOCATION}
-    echo "Starting Django server in directory ${ANGULAR_APP_LOCATION}"
 
-    nohup ng serve &
-  else
-    echo "variable ANGULAR_APP_LOCATION needs to be set to the path of the Angular application"
-
-    # kill the Django server and angular server if they are running
-    ps ax | grep "manage.py" | grep -v "grep" | awk {'print $1'} | xargs kill
-    ps ax | grep "ng serve" | grep -v "grep" | awk {'print $1'} | xargs kill
-
-    exit 2
-  fi
-
-else
-	echo "usage: startserevers.sh django-app-path angular-app-path"
-  exit 2
-fi
-
-#get backl to base directory
+#get back to base directory
 cd $APP_HOME
-
